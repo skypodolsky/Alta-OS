01856b22c83f2eb482b75109557c6b0ab1bb6e2c
StanislavPodolsky <mark_castle@mail.ru>
2014-06-22 03:06:22 +0300

    added keyboard driver, implemented time handling routine

---
+++ include/sys/time.h
@@ -0,0 +1,8 @@
+#ifndef TIME_HEADER
+#define TIME_HEADER
+
+#include <sys/types.h>
+
+extern uint64_t jiffies;
+
+#endif /* TIME_HEADER */
---
+++ kernel/time.c
@@ -0,0 +1,5 @@
+#include <sys/time.h>
+
+uint64_t jiffies;
+
+
